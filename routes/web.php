5bae3c7f5ff9cc9036d1ae3cc37a8dd631c0bb5b
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2024-11-01 22:46:54 +0530

    feat: add job details route to retrieve specific job information

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -39,6 +40,38 @@ Route::get('/jobs', function () {
             ]
         ]
     );
+});
+
+Route::get('/jobs/{id}', function ($id) {
+    $jobs = [
+        [
+            "id" => 1,
+            "title" => "Programmer",
+            "salary" => "$80,000",
+        ],
+        [
+            "id" => 2,
+            "title" => "Designer",
+            "salary" => "$70,000",
+        ],
+        [
+            "id" => 3,
+            "title" => "Developer",
+            "salary" => "$90,000",
+        ],
+        [
+            "id" => 4,
+            "title" => "Manager",
+            "salary" => "$100,000",
+        ],
+        [
+            "id" => 5,
+            "title" => "CEO",
+            "salary" => "$200,000",
+        ]
+    ];
+    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+    dd($job);
 });
 
 
