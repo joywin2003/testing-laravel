1e8883b4f47c3589e77d29ff2fb279ff5410f879
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2024-11-02 22:27:54 +0530

    add the fillable fields in model to ensure no laravel mass assignment error

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -5,15 +5,10 @@ namespace App\Models;
 use Illuminate\Support\Arr;
 use Illuminate\Database\Eloquent\Model;
 
-class Job extends Model{
+class Job extends Model
+{
 
 
     protected $table = 'job_listings';
-    // public static function find(int $id){
-    //     $jobs =  Arr::first(self::all(), fn($job) => $job['id'] == $id);
-    //     if(!$jobs){
-    //         abort(404);
-    //     }
-    //     return $jobs;
-    // }
-};
+    protected $fillable = ['title', 'salary'];
+}
