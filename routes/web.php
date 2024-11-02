a6f80e2719c955d2cd38b2aab2405d4e6d5faba6
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2024-11-02 13:01:09 +0530

    refactor: removed redundant jobs variable

--- routes/web.php
+++ routes/web.php
@@ -3,73 +3,50 @@
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
+
+$jobs = [
+    [
+        "id" => 1,
+        "title" => "Programmer",
+        "salary" => "$80,000",
+    ],
+    [
+        "id" => 2,
+        "title" => "Designer",
+        "salary" => "$70,000",
+    ],
+    [
+        "id" => 3,
+        "title" => "Developer",
+        "salary" => "$90,000",
+    ],
+    [
+        "id" => 4,
+        "title" => "Manager",
+        "salary" => "$100,000",
+    ],
+    [
+        "id" => 5,
+        "title" => "CEO",
+        "salary" => "$200,000",
+    ],
+];
+
+
+
 Route::get('/', function () {
     return view('home');
 });
 
-
-Route::get('/jobs', function () {
+Route::get('/jobs', function () use ($jobs) {
     return view('jobs',
         [
-            'jobs' => [
-                [
-                    "id" => 1,
-                    "title" => "Programmer",
-                    "salary" => "$80,000",
-                ],
-                [
-                    "id" => 2,
-                    "title" => "Designer",
-                    "salary" => "$70,000",
-                ],
-                [
-                    "id" => 3,
-                    "title" => "Developer",
-                    "salary" => "$90,000",
-                ],
-                [
-                    "id" => 4,
-                    "title" => "Manager",
-                    "salary" => "$100,000",
-                ],
-                [
-                    "id" => 5,
-                    "title" => "CEO",
-                    "salary" => "$200,000",
-                ]
-            ]
+            'jobs' => $jobs
         ]
     );
 });
 
-Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-        [
-            "id" => 1,
-            "title" => "Programmer",
-            "salary" => "$80,000",
-        ],
-        [
-            "id" => 2,
-            "title" => "Designer",
-            "salary" => "$70,000",
-        ],
-        [
-            "id" => 3,
-            "title" => "Developer",
-            "salary" => "$90,000",
-        ],
-        [
-            "id" => 4,
-            "title" => "Manager",
-            "salary" => "$100,000",
-        ],
-        [
-            "id" => 5,
-            "title" => "CEO",
-            "salary" => "$200,000",
-        ]
-    ];
+Route::get('/jobs/{id}', function ($id) use ($jobs) {
     $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
     // dd($job);
     return view('job', ['job' => $job]);
