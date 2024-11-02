8fe12a37fed750ac243868d3cc1dd8410b2091b0
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2024-11-02 13:37:39 +0530

    refactor: encapsulate job data in Job class and streamline job retrieval

--- routes/web.php
+++ routes/web.php
@@ -3,34 +3,37 @@
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
-
-$jobs = [
-    [
-        "id" => 1,
-        "title" => "Programmer",
-        "salary" => "$80,000",
-    ],
-    [
-        "id" => 2,
-        "title" => "Designer",
-        "salary" => "$70,000",
-    ],
-    [
-        "id" => 3,
-        "title" => "Developer",
-        "salary" => "$90,000",
-    ],
-    [
-        "id" => 4,
-        "title" => "Manager",
-        "salary" => "$100,000",
-    ],
-    [
-        "id" => 5,
-        "title" => "CEO",
-        "salary" => "$200,000",
-    ],
-];
+class Job {
+    public static function all(): array{
+        return [
+            [
+                "id" => 1,
+                "title" => "Programmer",
+                "salary" => "$80,000",
+            ],
+            [
+                "id" => 2,
+                "title" => "Designer",
+                "salary" => "$70,000",
+            ],
+            [
+                "id" => 3,
+                "title" => "Developer",
+                "salary" => "$90,000",
+            ],
+            [
+                "id" => 4,
+                "title" => "Manager",
+                "salary" => "$100,000",
+            ],
+            [
+                "id" => 5,
+                "title" => "CEO",
+                "salary" => "$200,000",
+            ],
+        ];
+    }
+};
 
 
 
@@ -38,15 +41,16 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/jobs', function () use ($jobs) {
+Route::get('/jobs', function () {
     return view('jobs',
         [
-            'jobs' => $jobs
+            'jobs' => Job::all()
         ]
     );
 });
 
-Route::get('/jobs/{id}', function ($id) use ($jobs) {
+Route::get('/jobs/{id}', function ($id) {
+    $jobs = Job::all();
     $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
     // dd($job);
     return view('job', ['job' => $job]);
