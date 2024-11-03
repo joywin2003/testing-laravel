f6d1b24889b5b5a67f734f5191e87b14e2ae5597
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2024-11-03 18:36:57 +0530

    feat: add Employer model and factory, update Job model and factory to include employer_id

--- database/factories/JobFactory.php
+++ database/factories/JobFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Employer;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -18,6 +19,7 @@ class JobFactory extends Factory
     {
         return [
             'title' => $this->faker->jobTitle,
+            "employer_id" => Employer::factory(),
             'salary' => $this->faker->numberBetween(30000, 120000),
         ];
     }
